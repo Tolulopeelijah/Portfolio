1d9215d3fe4a981df4b87f43271e740b87554fe0
Tolulope <toulopee90@gmail.com>
2023-03-17 10:11:56 +0100

    project next and previous button

--- portfolio project/javascript/portfolio.js
+++ portfolio project/javascript/portfolio.js
@@ -1,2 +1,14 @@
-console.log('All good, no errors');
-document.write("<h1>this is for testing</h1>");
+var x = 0;
+let topic_of_projects = ["Planet survival prediction - Neo's data", "Fiverr Spammer Detection", "<a href = 'www.com' alt = 'great motors'>Great Motors</a> price prediction", "more coming soon!"]
+function right_scroll (){
+    document.getElementById("project_topic").innerHTML = topic_of_projects[x];
+    if (x < topic_of_projects.length -1){
+        x++;
+    }
+}
+function left_scroll (){
+    document.getElementById("project_topic").innerHTML = topic_of_projects[x];
+    if (x != 0){
+        x--;
+    }
+}
